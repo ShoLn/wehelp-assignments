870895837ed5e5b66be412e21ebb8147f2a9dbe8
Sho Ln <learningsyo@gmail.com>
2022-02-19 22:43:01 +0800

    edit with connection pool

--- week-7/myproject/view/db.py
+++ week-7/myproject/view/db.py
@@ -13,35 +13,33 @@ class Db():
             database='website',
             auth_plugin="mysql_native_password"
         )
-        self.cnt_pool_obj = None
-        self.cursor = None
 
     def query(self, sql, val=None, one_row=True):
         try:
-            self.cnt_pool_obj = self.pool_object.get_connection()
-            self.cursor = self.cnt_pool_obj.cursor(dictionary=True,buffered=True)
-            self.cursor.execute(sql,val)
+            cnt_pool_obj = self.pool_object.get_connection()
+            cursor = cnt_pool_obj.cursor(dictionary=True,buffered=True)
+            cursor.execute(sql,val)
             if one_row:
-                data = self.cursor.fetchone()  # return none if no data
-                self.cursor.close()
-                self.cnt_pool_obj.close()
+                data = cursor.fetchone()  # return none if no data
+                cursor.close()
+                cnt_pool_obj.close()
                 return data
             else:
-                data = self.cursor.fetchall()  # return empty list if no data
-                self.cursor.close()
-                self.cnt_pool_obj.close()
+                data = cursor.fetchall()  # return empty list if no data
+                cursor.close()
+                cnt_pool_obj.close()
                 return data
         except mysql.connector.Error as e:
             print('query error', e)
 
     def change(self, sql, val):
         try:
-            self.cnt_pool_obj = self.pool_object.get_connection()
-            self.cursor = self.cnt_pool_obj.cursor(dictionary=True,buffered=True)
-            self.cursor.execute(sql, val)
-            self.cnt_pool_obj.commit()
-            self.cursor.close()
-            self.cnt_pool_obj.close()
+            cnt_pool_obj = self.pool_object.get_connection()
+            cursor = cnt_pool_obj.cursor(dictionary=True,buffered=True)
+            cursor.execute(sql, val)
+            cnt_pool_obj.commit()
+            cursor.close()
+            cnt_pool_obj.close()
         except mysql.connector.Error as e:
             print('change error', e)
 
